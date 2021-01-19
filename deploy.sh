cea8a83303dbdb511bae8197201ada4edaa978d1
Yevhenii Semendiak <semendyak@gmail.com>
2021-01-19 19:17:28 +0200

    rearrange, add imgs for testing inference

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
-# Run the gateway service
+# Cleaup all
 kubectl config use-context onprem-poc
-
-kubectl create namespace mlops-integrations
-neuro config switch-cluster neuro-compute
+kubectl create namespace mlops-integrations || echo exists!
 kubectl -n mlops-integrations delete secret neuro-token
-kubectl -n mlops-integrations create secret generic neuro-token --from-literal=neuro-compute-token=$(neuro config show-token)
-
-
-kubectl -n seldon get seldondeployment # my-model -o yaml
-
 kubectl -n seldon delete seldondeployment my-model
 kubectl delete -f k8s/seldon-roles.yaml
 kubectl delete -f k8s/deployment.yaml
 
-
+# Deploy
+kubectl config use-context onprem-poc
+kubectl create namespace mlops-integrations
+neuro config switch-cluster neuro-compute
+kubectl -n mlops-integrations create secret generic neuro-token --from-literal=neuro-compute-token=$(neuro config show-token)
 kubectl apply -f k8s/seldon-roles.yaml
 kubectl apply -f k8s/deployment.yaml
 
 
+# Verify
+# get seldon deployments
+# kubectl -n seldon get seldondeployment # my-model -o yaml
+# 
 kubectl -n mlops-integrations get po -l run=mlops-kube-gateway
+
 SERVICE_POD=$(kubectl -n mlops-integrations get po -l run=mlops-kube-gateway -o jsonpath='{.items[*].metadata.name}')
 echo SERVICE_POD=${SERVICE_POD}
 
-
 kubectl -n mlops-integrations logs -f ${SERVICE_POD}
+kubectl port-forward svc/seldon-core-analytics-grafana 3000:80 -n seldon-system

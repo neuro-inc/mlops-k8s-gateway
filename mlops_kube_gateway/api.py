d4ecb791dd21ef70fe05e79823673f4cff557bbd
Artem Yushkovsky <ajuszkowski@ya.ru>
2021-02-02 13:19:17 +0300

    Refactor (#3)

--- mlops_kube_gateway/api.py
+++ mlops_kube_gateway/api.py
@@ -36,7 +36,7 @@ def _load_json(url: str, **kwargs):
 async def _create_seldon_deployment(
     *,
     name: str,
-    neuro_passed_config: str,
+    seldon_neuro_passed_config: str,
     registry_secret_name: str,
     model_image_ref: str,
     model_storage_uri: str,
@@ -53,7 +53,7 @@ async def _create_seldon_deployment(
                 "image": "neuromation/neuro-extras:latest",
                 "imagePullPolicy": "Always",
                 "command": ["bash", "-c"],
-                "args": [f"neuro cp {model_storage_uri} /storage"],
+                "args": [f"neuro --verbose cp {model_storage_uri} /storage"],
                 "volumeMounts": [
                     {"mountPath": "/storage", "name": "neuro-storage"},
                 ],
@@ -63,7 +63,7 @@ async def _create_seldon_deployment(
                 "env": [
                     {
                         "name": "NEURO_PASSED_CONFIG",
-                        "value": neuro_passed_config,
+                        "value": seldon_neuro_passed_config,
                     }
                 ],
             }
@@ -101,6 +101,25 @@ async def _create_seldon_deployment(
         },
     }
 
+
+def _full_neuro_image_to_ref(image_uri: str) -> str:
+    pre = "image://"
+    if not image_uri.startswith(pre):
+        raise ValueError(f"Invalid neuro image: expected '{pre}...' got '{image_uri}'")
+    image_uri = image_uri[len(pre): ]
+    cluster, user, *rest = image_uri.split("/")
+    return f"registry.{cluster}.org.neu.ro/{user}/" + "/".join(rest)
+
+
+def _parse_cluster_name(storage_uri: str) -> str:
+    pre = "storage://"
+    if not storage_uri.startswith(pre):
+        raise ValueError(f"Invalid neuro storage: expected '{pre}...' got '{storage_uri}'")
+    storage_uri = storage_uri[len(pre): ]
+    cluster, user, *rest = storage_uri.split("/")
+    return cluster
+
+
 async def poll_mlflow(env):
     env = {
         k: v
@@ -110,81 +129,87 @@ async def poll_mlflow(env):
     env_str = "\n".join((f"{k}={v}" for k, v in env.items()))
     logger.info(f"Environment:\n{env_str}")
 
-    neuro_cp_token = env["MKG_NEURO_CP_TOKEN"]
-    project_name = env["MKG_NEURO_PROJECT_NAME"]
-
-    model_name = env.get("MKG_SELDON_MODEL_NAME", "my-model")
-    model_stage = env.get("MKG_SELDON_MODEL_STAGE", "Production")
-
-    mlflow_uri_base = env.get("MKG_MLFLOW_URI", "https://open-source-stack-mlflow-server--yevheniisemendiak.jobs.neuro-compute.org.neu.ro")
-    neuro_model_image_ref = env.get("MKG_NEURO_MODEL_IMAGE_REF", "registry.onprem-poc.org.neu.ro/yevheniisemendiak/startup_package_test/seldon:20.12.16")
-    neuro_cluster = env.get("MKG_NEURO_CLUSTER", "neuro-compute")
-    neuro_user = env.get("MKG_NEURO_USER", "yevheniisemendiak")
-
-    neuro_passed_config = base64.b64encode(
+    # Settings of the first cluster where MLflow is deployed (neuro-compute):
+    mlflow_neuro_user = env["MKG_MLFLOW_NEURO_USER"]
+    mlflow_neuro_cp_token = env["MKG_MLFLOW_NEURO_CP_TOKEN"]
+    mlflow_neuro_job_name = env["MKG_MLFLOW_NEURO_JOB_NAME"]
+    mlflow_neuro_project_storage = env["MKG_MLFLOW_NEURO_PROJECT_STORAGE"]
+    mlflow_neuro_cluster = _parse_cluster_name(mlflow_neuro_project_storage)
+    mlflow_uri_base = f"https://{mlflow_neuro_job_name}--{mlflow_neuro_user}.jobs.{mlflow_neuro_cluster}.org.neu.ro"
+    mlflow_url = mlflow_uri_base + "/api/2.0/preview/mlflow"
+    seldon_neuro_passed_config = base64.b64encode(
         json.dumps(
             {
-                "token": neuro_cp_token,
-                "cluster": "neuro-compute",
+                "token": mlflow_neuro_cp_token,
+                "cluster": mlflow_neuro_cluster,
                 "url": "https://staging.neu.ro/api/v1",
             }
         ).encode()
     ).decode()
 
-    mlflow_url = mlflow_uri_base + "/api/2.0/preview/mlflow"
+    # Settings of the second cluster where Seldon is deployed (onprem-poc):
+    seldon_model_name = env.get("MKG_SELDON_MODEL_NAME", "my-model")
+    seldon_model_stage = env.get("MKG_SELDON_MODEL_STAGE", "Production")
+    seldon_model_file_name = env.get("MKG_SELDON_MODEL_FILE_NAME", "model.pth")
+    seldon_neuro_registry_secret_name = env.get("MKG_SELDON_NEURO_REGISTRY_SECRET_NAME", "neuro-registry")
+    seldon_neuro_image = env.get("MKG_SELDON_NEURO_IMAGE", f"image://onprem-poc/artemyushkovsky/ml_recipe_bone_age/seldon:21.1.23")
+    seldon_neuro_image_ref = _full_neuro_image_to_ref(seldon_neuro_image)
 
-    delay = 1
+    DELAY = 5
 
     prev_version = None
-    model_versions_uri = f"{mlflow_url}/registered-models/get-latest-versions?name={model_name}&stages={model_stage}"
-    logger.info(f"Starting polling {model_versions_uri} with delay {delay} sec")
+    model_versions_uri = f"{mlflow_url}/registered-models/get-latest-versions?name={seldon_model_name}&stages={seldon_model_stage}"
+    logger.info(f"Starting polling {model_versions_uri} with delay {DELAY} sec")
     while True:
         try:
             async with aiohttp.ClientSession() as session:
-                await asyncio.sleep(delay)
-                logger.info(f"Polling model {model_name} stage {model_stage}")
+                await asyncio.sleep(DELAY)
+                logger.info(f"Polling {mlflow_uri_base}: model '{seldon_model_name}' stage '{seldon_model_stage}'")
                 async with session.get(model_versions_uri) as resp:
                     assert resp.status == 200, (resp.status, await resp.text())
                     data = await resp.json()
                     model_metadata = data["model_versions"][0]
                     model_version = model_metadata["version"]
-                    if prev_version is None or model_version != prev_version:
+
+                    run_info = ""
+                    model_metrics = {}
+                    try:
+                        run_id = model_metadata["run_id"]
+                        model_metrics_uri = f"{mlflow_url}/runs/get?run_id={run_id}"
+                        async with session.get(model_metrics_uri) as r:
+                            assert r.status == 200, (r.status, await r.text())
+                            metrics_data = await r.json()
+                            metrics_data = metrics_data["run"]
+                            run_info = metrics_data["info"]
+                            run_id = run_info["run_id"]
+                            # model_params = metrics_data["data"]["params"]
+                            model_metrics = metrics_data["data"]["metrics"]
+                    except asyncio.CancelledError:
+                        raise
+                    except BaseException as e:
+                        logger.warning(f"Could not get model metrics: {e}")
+
+
+                    if prev_version is not None and model_version == prev_version:
+                        logger.info(f"Already deployed: model version={model_version} run_id={run_id}")
+                    else:
                         logger.info("")
-                        logger.info(f"Deploying model version={model_version}")
+                        logger.info(f"Deploying model version={model_version} run_id={run_id}")
                         logger.info(f"Model metadata:\n{yaml.dump(model_metadata)}")
-                        try:
-                            run_id = model_metadata["run_id"]
-                            model_metrics_uri = f"{mlflow_url}/runs/get?run_id={run_id}"
-                            async with session.get(model_metrics_uri) as r:
-                                assert r.status == 200, (r.status, await r.text())
-                                metrics_data = await r.json()
-                                metrics_data = metrics_data["run"]
+                        # logger.info(f"Model run info:\n{yaml.dump(run_info)}")
+                        logger.info(f"Model metrics:\n{yaml.dump(model_metrics)}")
+                        # logger.info(f"Model params:\n{yaml.dump(model_params)}")
 
-                                run_info = metrics_data["info"]
-                                logger.info(f"Model run info:\n{yaml.dump(run_info)}")
-
-                                model_params = metrics_data["data"]["params"]
-                                logger.info(f"Model params:\n{yaml.dump(model_params)}")
-
-                                model_metrics = metrics_data["data"]["metrics"]
-                                logger.info(f"Model metrics:\n{yaml.dump(model_metrics)}")
-                        except asyncio.CancelledError:
-                            raise
-                        except BaseException as e:
-                            logger.warning(f"Could not get model metrics: {e}")
-
-                        logger.info(f"Model")
                         model_source = model_metadata['source']
                         assert model_source.startswith('/usr/local/share/'), model_source
                         assert model_source.endswith('/artifacts/model'), model_source
                         model_subpath = model_source[len('/usr/local/share/'):]
-                        model_storage_uri = f"storage://{neuro_cluster}/{neuro_user}/{project_name}/{model_subpath}/data/model.h5"
-
+                        model_storage_uri = f"{mlflow_neuro_project_storage}/{model_subpath}/data/{seldon_model_file_name}"
                         deployment_json = await _create_seldon_deployment(
-                            name=model_name,
-                            neuro_passed_config=neuro_passed_config,
-                            registry_secret_name="neuro-registry",
-                            model_image_ref=neuro_model_image_ref,
+                            name=seldon_model_name,
+                            seldon_neuro_passed_config=seldon_neuro_passed_config,
+                            registry_secret_name=seldon_neuro_registry_secret_name,
+                            model_image_ref=seldon_neuro_image_ref,
                             model_storage_uri=model_storage_uri,
                         )
                         deployment = yaml.dump(deployment_json)
@@ -192,10 +217,8 @@ async def poll_mlflow(env):
                         path = Path(tempfile.mktemp())
                         path.write_text(deployment)
                         subprocess.run(f"kubectl apply -f {path}", shell=True, check=True)
-                        # import asyncio.subprocess
-                        # asyncio.subprocess.run(f"kubectl apply -f {path}", shell=True, check=True)
                         prev_version = model_version
-                        # logger.info(f"Successfully deployed, from temp path {path}")
+                        logger.info(f"Model version={model_version} successfully deployed!")
                     # else:
                     #     logger.info(f"Found same version: {model_version}")
 
@@ -204,12 +227,13 @@ async def poll_mlflow(env):
         except BaseException as e:
             logger.warning(f"Unexpected exception (ignoring): {e}")
         finally:
-            # logger.info(f"Deleting seldon deployment {model_name}...")
-            # subprocess.run(f"kubectl -n seldon delete {model_name}", shell=True, check=True)
+            # logger.info(f"Deleting seldon deployment {seldon_model_name}...")
+            # subprocess.run(f"kubectl -n seldon delete {seldon_model_name}", shell=True, check=True)
             # logger.info(f"Deleted")
             pass
 
 def main():
     logging.basicConfig(level=logging.DEBUG, 
-                        format='%(asctime)s %(levelname)s %(message)s')
+                        format='%(asctime)s %(levelname)s %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
     asyncio.run(poll_mlflow(os.environ))
